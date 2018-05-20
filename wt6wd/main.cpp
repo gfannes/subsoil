242de2a5580ff13588d4887e60cb546f669ae738
Geert Fannes <geert.fannes@gmail.com>
2018-05-20 10:29:06 +0200

    wip

--- wt6wd/main.cpp
+++ wt6wd/main.cpp
@@ -1,4 +1,5 @@
 #include "Arduino.h"
+#include "Servo.h"
 
 template <unsigned int Pin, unsigned int Min, unsigned int Max>
 class PPMPin
@@ -46,18 +47,25 @@ PPMPin<3 , Min, Max> ch2;
 PPMPin<18, Min, Max> ch3;
 PPMPin<19, Min, Max> ch4;
 
+Servo servo;
+
 void setup() {
     Serial.begin(9600);
     ch1.setup();
     ch2.setup();
     ch3.setup();
     ch4.setup();
+
+    /* servo.attach(9); */
+    pinMode(9, OUTPUT);
 }
 
 void loop() {
-    Serial.print(ch1.as_float()); Serial.print("\t");
-    Serial.print(ch2.as_float()); Serial.print("\t");
-    Serial.print(ch3.as_float()); Serial.print("\t");
-    Serial.print(ch4.as_float()); Serial.println("");
+    /* Serial.print(ch1.as_float()); Serial.print("\t"); */
+    /* Serial.print(ch2.as_float()); Serial.print("\t"); */
+    /* Serial.print(ch3.as_float()); Serial.print("\t"); */
+    /* Serial.print(ch4.as_float()); Serial.println(""); */
+    /* servo.write(ch1.as_float()*180); */
+    analogWrite(9, ch1.as_float()*255);
     /* delay(200); */
 }
