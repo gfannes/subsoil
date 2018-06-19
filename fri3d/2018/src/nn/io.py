0ea519d908c897e5c1b8d5ca24de480ff6ce7980
Geert Fannes <geert.fannes@gmail.com>
2018-06-19 18:31:31 +0200

    wip

--- fri3d/2018/src/nn/io.py
+++ fri3d/2018/src/nn/io.py
@@ -21,12 +21,11 @@ class Perceptron:
         self.simulator = Simulator()
         self.ui = self.UI()
 
-        simulator = self.simulator
-        self.input = simulator.add_external(1)
-        self.bias  = simulator.add_external(1)
-        self.output, self.weight = simulator.add_neuron("tanh", [self.input, self.bias])
-        self.states = np.zeros(simulator.nr_states)
-        self.weights = np.zeros(simulator.nr_weights)
+        self.input = self.simulator.add_external(1)
+        self.bias  = self.simulator.add_external(1)
+        self.output, self.weight = self.simulator.add_neuron("tanh", [self.input, self.bias])
+        self.states = np.zeros(self.simulator.nr_states)
+        self.weights = np.zeros(self.simulator.nr_weights)
         self.states[self.bias] = 1.0
 
     def show(self, ctx):
@@ -42,7 +41,7 @@ class Perceptron:
         bimpy.slider_float("Input", ui.input, *mm)
 
         self._compute_outputs()
-        bimpy.plot_lines("", self._outputs, 0, "", -1.0, 1.0, bimpy.Vec2(0,100))
+        bimpy.plot_lines("", self.outputs, 0, "", -1.0, 1.0, bimpy.Vec2(0,100))
 
         bimpy.same_line()
 
@@ -54,7 +53,7 @@ class Perceptron:
         self.simulator.forward(self.states, self.weights)
         return self.states[self.output]
     def _compute_outputs(self):
-        self._outputs = [self._compute_output(ix2f(ix)) for ix in range(NR+1)]
+        self.outputs = [self._compute_output(ix2f(ix)) for ix in range(NR+1)]
 
 class MLP:
     class UI:
@@ -67,6 +66,7 @@ class MLP:
     def __init__(self):
         self.ui = self.UI()
         self.mlp = gubg_MLP(1)
+        self.simulator = None
     def show(self, cxt):
         ui = self.ui
 
@@ -74,17 +74,17 @@ class MLP:
         bimpy.input_int("nr neurons", ui.nr)
         bimpy.same_line()
         bimpy.combo("transfer", ui.transfer, ui.transfer_names)
-        bimpy.same_line()
-        if (bimpy.button("add layer")):
-            transfer = ui.transfer_names[ui.transfer.value]
-            self.mlp.add_layer(transfer, ui.nr.value)
+        if (ui.nr.value > 0):
+            bimpy.same_line()
+            if (bimpy.button("add layer")):
+                transfer = ui.transfer_names[ui.transfer.value]
+                self.mlp.add_layer(transfer, ui.nr.value)
+                self.simulator = None
         bimpy.pop_item_width()
 
         if (not self.mlp.layers):
             bimpy.text("Warning: you need at least one layer")
             return
-        if (self.mlp.layers[-1].nr_outputs != 1):
-            bimpy.text("Warning: last layer should have only one output")
 
         names = ["{ix} {str}".format(ix=ix, str=str(layer)) for ix,layer in enumerate(self.mlp.layers)]
         bimpy.combo("Layer", ui.layer, names)
@@ -92,3 +92,28 @@ class MLP:
         layer_ix = ui.layer.value
         if (layer_ix < len(self.mlp.layers)):
             layer = self.mlp.layers[layer_ix]
+
+        if (self.mlp.layers[-1].nr_outputs != 1):
+            bimpy.text("Warning: last layer should have only one output")
+            return
+
+        if (not self.simulator):
+            self.simulator = Simulator()
+            self.mlp.setup_simulator(self.simulator)
+            self.states = np.zeros(self.simulator.nr_states)
+            self.weights = np.zeros(self.simulator.nr_weights)
+            self.output = self.simulator.nr_states-1
+            self.states[self.mlp.bias] = 1.0
+        bimpy.text(str(self.simulator))
+
+        self._compute_outputs()
+        print(self.outputs)
+        bimpy.plot_lines("Function", self.outputs)
+
+    def _compute_output(self, input):
+        self.states[self.mlp.input0] = input
+        self.simulator.forward(self.states, self.weights)
+        return self.states[self.output]
+    def _compute_outputs(self):
+        self.outputs = [self._compute_output(ix2f(ix)) for ix in range(NR+1)]
+
