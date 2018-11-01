a12f01bbb7e954d5a2047129faaa326c0013cbca
Geert Fannes <geert.fannes@gmail.com>
2018-11-01 13:24:58 +0100

    Reversed Y-axis

--- fri3d/2018/src/mlp.cpp
+++ fri3d/2018/src/mlp.cpp
@@ -38,7 +38,7 @@ class Transform
 {
 public:
     template <typename Window>
-    Transform(const Window &wnd, double x, double y): x_(x, wnd.getSize().x), y_(y, wnd.getSize().y) {}
+    Transform(const Window &wnd, double x, double y): x_(x, wnd.getSize().x), y_(-y, wnd.getSize().y) {}
     sf::Vector2f operator()(double x, double y) const {return sf::Vector2f(x_(x), y_(y));}
 private:
     Linear x_, y_;
