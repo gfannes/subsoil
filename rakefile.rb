934f8f1fa2c010b525ad5fe0816a5924fed53124
Geert Fannes <geert.fannes@gmail.com>
2018-11-01 22:40:48 +0100

    Added crosstool-NG experiment

--- rakefile.rb
+++ rakefile.rb
@@ -86,7 +86,7 @@ namespace :fri3d do
     end
     desc "MLP test app"
     task :mlp do
-        cooker().option("release").generate(:ninja, "fri3d/mlp").ninja().run()
+        cooker().option("release").option("ctng.tuple", "i686-unknown-linux-gnu").generate(:ninja, "fri3d/mlp").ninja(1).run()
     end
     desc "Create MLPs"
     task :create_mlps do
