d01dd0dd5118f44cd463fc0c05774e68df62b42d
Geert Fannes <geert.fannes@gmail.com>
2019-04-22 11:53:40 +0200

    Added gubg::arduino::ppm::Pin demo

---
+++ poc/ppm.cpp
@@ -0,0 +1,21 @@
+#include <gubg/arduino/ppm/Pin.hpp>
+using namespace gubg::arduino;
+
+const unsigned int Min = 1050;
+const unsigned int Max = 1850;
+ppm::Pin<2 , Min, Max> ch1;
+ppm::Pin<3 , Min, Max> ch2;
+
+void setup()
+{
+    ch1.setup();
+    ch2.setup();
+    Serial.begin(9600);
+}
+
+void loop()
+{
+    Serial.print(ch1.as_float());
+    Serial.print(' ');
+    Serial.println(ch2.as_float());
+}
--- wt6wd/main.cpp
+++ wt6wd/main.cpp
@@ -1,51 +1,14 @@
+#include <gubg/arduino/ppm/Pin.hpp>
 #include "Arduino.h"
 #include "Servo.h"
-
-template <unsigned int Pin, unsigned int Min, unsigned int Max>
-class PPMPin
-{
-public:
-    void setup()
-    {
-        pinMode(Pin, INPUT);
-        attachInterrupt(digitalPinToInterrupt(Pin), process_pin_change_, CHANGE);
-        prev_ = 0;
-        pulse_duration_ = 0;
-    }
-
-    unsigned int diff() const { return pulse_duration_; }
-    float as_float() const { return float(pulse_duration_-Min)/float(Max-Min); }
-
-private:
-    static void process_pin_change_()
-    {
-        const unsigned int now = micros();
-        if (digitalRead(Pin) == HIGH)
-            prev_ = now;
-        else
-        {
-            pulse_duration_ = now-prev_;
-            if (pulse_duration_ < Min)
-                pulse_duration_ = Min;
-            else if (pulse_duration_ > Max)
-                pulse_duration_ = Max;
-        }
-    }
-
-    static unsigned int prev_;
-    static unsigned int pulse_duration_;
-};
-template <unsigned int Pin, unsigned int Min, unsigned int Max>
-unsigned int PPMPin<Pin, Min, Max>::prev_ = 0;
-template <unsigned int Pin, unsigned int Min, unsigned int Max>
-unsigned int PPMPin<Pin, Min, Max>::pulse_duration_ = 0;
+using namespace gubg::arduino;
 
 const unsigned int Min = 1050;
 const unsigned int Max = 1850;
-PPMPin<2 , Min, Max> ch1;
-PPMPin<3 , Min, Max> ch2;
-PPMPin<18, Min, Max> ch3;
-PPMPin<19, Min, Max> ch4;
+ppm::Pin<2 , Min, Max> ch1;
+ppm::Pin<3 , Min, Max> ch2;
+ppm::Pin<18, Min, Max> ch3;
+ppm::Pin<19, Min, Max> ch4;
 
 Servo servo;
 
