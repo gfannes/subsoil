8db665b428b84706e7be1ee876e40737ce790222
Geert Fannes <geert.fannes@gmail.com>
2020-08-01 16:29:26 +0200

    Timer is integrated

--- laurot/src/test/main.cpp
+++ laurot/src/test/main.cpp
@@ -1,30 +1,73 @@
+#include <gubg/arduino/Pin.hpp>
+#include <gubg/arduino/Elapsed.hpp>
+#include <gubg/std/array.hpp>
+#include <gubg/std/algorithm.hpp>
+
 #define gubg_no_log 0
 #include "gubg/log.hpp"
 
 #include "Arduino.h"
 
-template <typename Ftor>
-void each_pin(Ftor &&ftor)
+class Relay
 {
-    for (auto pin = 46u; pin < 54u; ++pin)
-        ftor(pin);
-}
+public:
+    void set_pin(std::uint8_t pin) { pin_.set_pin(pin).set_output(true); }
+
+    void set_duration(unsigned int ms) {duration_ms_ = ms;}
+
+    void activate() { active_ms_ = duration_ms_; }
+
+    void process(unsigned int elapse_ms)
+    {
+        const bool active = active_ms_ > 0;
+        pin_.set_output(!active);
+
+        elapse_ms = std::min(elapse_ms, active_ms_);
+        active_ms_ -= elapse_ms;
+    }
+private:
+    gubg::arduino::Pin pin_;
+    unsigned int duration_ms_ = 2000;
+    unsigned int active_ms_ = 0;
+};
+
+class Button
+{
+public:
+    void set_pin(std::uint8_t pin) { pin_.set_pin(pin).set_input(true); }
+
+    bool pressed() { return pin_.check().is_input(false); }
+private:
+    gubg::arduino::Pin pin_;
+};
+
+std::array<Relay, 7> relays{};
+std::array<Button, 7> buttons{};
+
+gubg::arduino::Elapsed<unsigned int> elapsed_ms;
 
 void setup()
 {
-    each_pin([](auto pin){pinMode(pin, OUTPUT);});
-    each_pin([](auto pin){digitalWrite(pin, true);});
+    for (auto ix = 0u; ix < 7; ++ix)
+    {
+        buttons[ix].set_pin(34+ix);
+        relays[ix].set_pin(46+ix);;
+    }
 }
 
 void loop()
 {
-    auto blink = [](auto pin)
+    elapsed_ms.process(millis());
+
+    for (auto ix = 0u; ix < 7; ++ix)
     {
-        digitalWrite(pin, false);
-        delay(500);
-        digitalWrite(pin, true);
-        delay(500);
-    };
-    each_pin(blink);
+        auto &button = buttons[ix];
+        auto &relay = relays[ix];
+
+        if (button.pressed())
+            relay.activate();
+
+        relay.process(elapsed_ms());
+    }
 }
 
