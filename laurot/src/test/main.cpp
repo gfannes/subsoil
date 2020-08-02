25217813cb925868d7c8103e31f8a2f2b999b587
Geert Fannes <geert.fannes@gmail.com>
2020-08-02 11:33:27 +0200

    Added support for debouncing and events

--- laurot/src/test/main.cpp
+++ laurot/src/test/main.cpp
@@ -8,65 +8,167 @@
 
 #include "Arduino.h"
 
-class Relay
+enum class Type {None, Button, Relay};
+
+class IO
 {
 public:
-    void set_pin(std::uint8_t pin) { pin_.set_pin(pin).set_output(true); }
+    //Button API
+    void setup_button(std::uint8_t pin)
+    {
+        type_ = Type::Button;
+        pin_.set_pin(pin).set_input(true);
+    }
+    bool button_is_pressed() const
+    {
+        if (!is(Type::Button))
+            return false;
+        return button_.is_pressed;
+    }
+    bool get_button_event(bool &pressed) const
+    {
+        if (!is(Type::Button))
+            return false;
+        if (!button_.has_event)
+            return false;
+        pressed = button_.is_pressed;
+        return true;
+    }
 
-    void set_duration(unsigned int ms) {duration_ms_ = ms;}
+    //Relay API
+    void setup_relay(std::uint8_t pin, unsigned int ms)
+    {
+        type_ = Type::Relay;
+        pin_.set_pin(pin).set_output(true);
+        relay_.duration_ms = ms;
+    }
+    bool relay_is_active() const
+    {
+        if (!is(Type::Relay))
+            return;
+        return relay_.active_ms > 0;
+    }
+    void activate_relay()
+    {
+        if (!is(Type::Relay))
+            return;
+        relay_.active_ms = relay_.duration_ms;
+    }
+    void deactivate_relay()
+    {
+        if (!is(Type::Relay))
+            return;
+        relay_.active_ms = 0;
+    }
 
-    void activate() { active_ms_ = duration_ms_; }
+    //Common API
+    bool is(Type type) const {return type_ == type;}
 
     void process(unsigned int elapse_ms)
     {
-        const bool active = active_ms_ > 0;
-        pin_.set_output(!active);
+        switch (type_)
+        {
+            case Type::Button:
+                {
+                    button_.has_event = false;
 
-        elapse_ms = std::min(elapse_ms, active_ms_);
-        active_ms_ -= elapse_ms;
+                    pin_.check();
+                    const auto state_ix = pin_.is_input(true) ? Button::UpIX : Button::DownIX;
+                    if (button_.debounce_counts[state_ix] < Button::DebounceCount)
+                    {
+                        //Button is still debouncing
+                        ++button_.debounce_counts[state_ix];
+                        button_.debounce_counts[1u-state_ix] = 0;
+                    }
+                    else
+                    {
+                        //Button reached stable state
+                        const auto new_pressed = (state_ix == Button::DownIX);
+                        if (new_pressed != button_.is_pressed)
+                        {
+                            button_.is_pressed = new_pressed;
+                            button_.has_event = true;
+                        }
+                    }
+                }
+                break;
+            case Type::Relay:
+                {
+                    pin_.set_output(!relay_is_active());
+
+                    elapse_ms = std::min(elapse_ms, relay_.active_ms);
+                    relay_.active_ms -= elapse_ms;
+                }
+                break;
+            default:
+                break;
+        }
     }
+
 private:
     gubg::arduino::Pin pin_;
-    unsigned int duration_ms_ = 2000;
-    unsigned int active_ms_ = 0;
+
+    Type type_ = Type::None;
+
+    struct Button
+    {
+        const static std::uint8_t DebounceCount = 16;
+        const static std::uint8_t UpIX = 0u;
+        const static std::uint8_t DownIX = 1u;
+
+        bool has_event = false;
+        bool is_pressed = false;
+        std::array<std::uint8_t, 2> debounce_counts{};
+    };
+    Button button_;
+
+    struct Relay
+    {
+        unsigned int duration_ms = 2000;
+        unsigned int active_ms = 0;
+    };
+    Relay relay_;
 };
 
-class Button
-{
-public:
-    void set_pin(std::uint8_t pin) { pin_.set_pin(pin).set_input(true); }
 
-    bool pressed() { return pin_.check().is_input(false); }
-private:
-    gubg::arduino::Pin pin_;
-};
-
-std::array<Relay, 2*7> relays{};
-std::array<Button, 2*7> buttons{};
+std::array<IO, 63> ios{};
 
 gubg::arduino::Elapsed<unsigned int> elapsed_ms;
 
 void setup()
 {
-    for (auto ix = 0u; ix < buttons.size(); ++ix)
-        buttons[ix].set_pin(26+ix);
-    for (auto ix = 0u; ix < relays.size(); ++ix)
-        relays[ix].set_pin(40+ix);;
+    for (auto ix = 0u; ix < 7; ++ix)
+        ios[ix].setup_button(A0+ix);
+    for (auto ix = 7u; ix < 14; ++ix)
+        ios[ix].setup_relay(A0+ix, 2000);
 }
 
 void loop()
 {
     elapsed_ms.process(millis());
 
-    for (auto ix = 0u; ix < std::min(buttons.size(), relays.size()); ++ix)
+    for (auto ix = 0u; ix < ios.size(); ++ix)
     {
-        auto &button = buttons[ix];
-        auto &relay = relays[ix];
+        auto &io = ios[ix];
 
-        if (button.pressed())
-            relay.activate();
+        bool pressed;
+        if (io.get_button_event(pressed))
+        {
+            const auto relay_ix = ix+7;
+            if (relay_ix < ios.size())
+            {
+                auto &relay = ios[relay_ix];
+                if (pressed)
+                {
+                    if (relay.relay_is_active())
+                        relay.deactivate_relay();
+                    else
+                        relay.activate_relay();
+                }
+            }
+        }
 
-        relay.process(elapsed_ms());
+        io.process(elapsed_ms());
     }
 }
 
