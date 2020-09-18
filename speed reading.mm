d613fdbc75b4afb2a288c5b19e9b32ef105a9477
Geert Fannes <geert.fannes@gmail.com>
2020-09-18 16:06:33 +0200

    wip

---
+++ speed reading.mm
@@ -0,0 +1,193 @@
+<map version="freeplane 1.8.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="speed reading" FOLDED="false" ID="ID_967658051" CREATED="1600423046256" MODIFIED="1600423058924" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="9" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="minder verklanken" POSITION="right" ID="ID_706763637" CREATED="1600427556220" MODIFIED="1600427560755">
+<edge COLOR="#0000ff"/>
+<node TEXT="affirmatie" ID="ID_601564902" CREATED="1600427512531" MODIFIED="1600427573683">
+<node TEXT="herhaal &quot;ik kan goed snellezen&quot; terwijl je leest" ID="ID_1954409290" CREATED="1600427518433" MODIFIED="1600427530226"/>
+</node>
+</node>
+<node TEXT="fotografisch lezen" POSITION="right" ID="ID_372042120" CREATED="1600429807827" MODIFIED="1600429813321">
+<edge COLOR="#00ff00"/>
+<node TEXT="beter snelle en kleine sprongen dan grote trage" ID="ID_366436773" CREATED="1600429815865" MODIFIED="1600429837629"/>
+</node>
+<node TEXT="typische snelheid" POSITION="right" ID="ID_1980283769" CREATED="1600431111521" MODIFIED="1600431120576">
+<edge COLOR="#ff00ff"/>
+<node TEXT="5 woorden per 0.25s" ID="ID_1070103318" CREATED="1600431121340" MODIFIED="1600431131599"/>
+</node>
+<node TEXT="essentielezen" POSITION="right" ID="ID_1282801332" CREATED="1600435089431" MODIFIED="1600435093048">
+<edge COLOR="#00007c"/>
+<node TEXT="leesdoel bepalen" ID="ID_119607621" CREATED="1600435223072" MODIFIED="1600435227510">
+<node TEXT="wat wil je bereiken door het boek te lezen?" ID="ID_979749057" CREATED="1600437602260" MODIFIED="1600437614503"/>
+</node>
+<node TEXT="achterflap, inhoudtabel, ..." ID="ID_646528054" CREATED="1600435094153" MODIFIED="1600435105933"/>
+<node TEXT="hoofdstukken selecteren" ID="ID_1908131052" CREATED="1600435107595" MODIFIED="1600435116012"/>
+<node TEXT="mindmap aanmaken, tak per hoofdstuk" ID="ID_1203451298" CREATED="1600435148537" MODIFIED="1600435159161"/>
+<node TEXT="samenvatting/conclusie per hoofdstuk" ID="ID_1115280358" CREATED="1600435116682" MODIFIED="1600435202810"/>
+<node TEXT="eerste en laatste alinea" ID="ID_169166265" CREATED="1600435124237" MODIFIED="1600435133397"/>
+<node TEXT="eerste en laatste zin" ID="ID_995595947" CREATED="1600435135496" MODIFIED="1600435143051"/>
+<node TEXT="herhaal mindmaps van tijd tot tijd" ID="ID_671497292" CREATED="1600437707485" MODIFIED="1600437716316"/>
+</node>
+<node TEXT="ml" POSITION="right" ID="ID_1761202071" CREATED="1600431283109" MODIFIED="1600431288116">
+<edge COLOR="#00ffff"/>
+<node TEXT="poly regression" ID="ID_938386328" CREATED="1600431296196" MODIFIED="1600431300989">
+<node TEXT="all combinations of features upto certain degree" ID="ID_702800657" CREATED="1600431308221" MODIFIED="1600431320793"/>
+<node TEXT="how to decide model complexity?" ID="ID_425213353" CREATED="1600431326019" MODIFIED="1600431332442"/>
+<node TEXT="underfitting" ID="ID_1654739890" CREATED="1600431368866" MODIFIED="1600431371913">
+<node TEXT="train and test curves show plateau" ID="ID_982129751" CREATED="1600431372584" MODIFIED="1600431392826"/>
+<node TEXT="curves are close" ID="ID_928788558" CREATED="1600431394370" MODIFIED="1600431401818"/>
+<node TEXT="curves are high" ID="ID_1972635743" CREATED="1600431402702" MODIFIED="1600431407272"/>
+</node>
+<node TEXT="overfitting" ID="ID_1196483657" CREATED="1600431408001" MODIFIED="1600431421585">
+<node TEXT="error on training is low" ID="ID_1876977291" CREATED="1600431429478" MODIFIED="1600431441448"/>
+<node TEXT="gap between the curves" ID="ID_1082264944" CREATED="1600431446154" MODIFIED="1600431454808"/>
+</node>
+</node>
+<node TEXT="generalization error" ID="ID_1344066613" CREATED="1600431465649" MODIFIED="1600431471269">
+<node TEXT="bias: wrong assumptions" ID="ID_705754205" CREATED="1600431472070" MODIFIED="1600431478504"/>
+<node TEXT="variance: sensitivity to small variations in training data" ID="ID_769461153" CREATED="1600431478739" MODIFIED="1600431496526"/>
+<node TEXT="irreducible error: noise in data" ID="ID_1478691986" CREATED="1600431497845" MODIFIED="1600431506251">
+<node TEXT="collect more data or clean it" ID="ID_153442956" CREATED="1600431509529" MODIFIED="1600431516754"/>
+</node>
+</node>
+<node TEXT="regularized linear models" ID="ID_1257987691" CREATED="1600431533969" MODIFIED="1600431543400">
+<node TEXT="ridge regression: L2" ID="ID_1156395518" CREATED="1600431544390" MODIFIED="1600431549951">
+<node TEXT="scale the data" ID="ID_1038617213" CREATED="1600431564864" MODIFIED="1600431576160"/>
+<node TEXT="has closed-form formula" ID="ID_123553721" CREATED="1600431580206" MODIFIED="1600431587077"/>
+</node>
+<node TEXT="lasso regression: L1" ID="ID_1373443630" CREATED="1600431588621" MODIFIED="1600431593179">
+<node TEXT="performs feature selection" ID="ID_66395546" CREATED="1600431604670" MODIFIED="1600431611897"/>
+</node>
+</node>
+</node>
+<node TEXT="coaching habit" POSITION="right" ID="ID_163024669" CREATED="1600435414610" MODIFIED="1600435420854">
+<edge COLOR="#007c00"/>
+<node TEXT="irresistible 123" ID="ID_113258642" CREATED="1600435421644" MODIFIED="1600435429741"/>
+<node TEXT="foundation question" ID="ID_56819506" CREATED="1600435429975" MODIFIED="1600435435430"/>
+<node TEXT="strategic question" ID="ID_1404384912" CREATED="1600435435628" MODIFIED="1600435440387"/>
+<node TEXT="learning question" ID="ID_329963566" CREATED="1600435441789" MODIFIED="1600435444859"/>
+</node>
+<node TEXT="deep work" POSITION="right" ID="ID_849699910" CREATED="1600435629765" MODIFIED="1600435631775">
+<edge COLOR="#7c007c"/>
+<node TEXT="goal" ID="ID_1145123351" CREATED="1600435840379" MODIFIED="1600435843013">
+<node TEXT="get started with deep work" ID="ID_659596996" CREATED="1600435847212" MODIFIED="1600435858556"/>
+</node>
+<node TEXT="rules" ID="ID_1405270769" CREATED="1600435643429" MODIFIED="1600435653557">
+<node TEXT="work deep 95" ID="ID_982489272" CREATED="1600435654392" MODIFIED="1600435822597">
+<node TEXT="depth style" ID="ID_1691920040" CREATED="1600436052524" MODIFIED="1600436057931">
+<node TEXT="monastic" ID="ID_1071341228" CREATED="1600436077482" MODIFIED="1600436079998">
+<node TEXT="contribution is discrete, clear and individual" ID="ID_600063075" CREATED="1600436161006" MODIFIED="1600436173989"/>
+</node>
+<node TEXT="bimodal" ID="ID_1842769970" CREATED="1600436090003" MODIFIED="1600436092330">
+<node TEXT="minimum deep work time: 1d" ID="ID_519625347" CREATED="1600436237920" MODIFIED="1600436248805"/>
+<node TEXT="divide time like Jung" ID="ID_1275039982" CREATED="1600436256153" MODIFIED="1600436276630"/>
+</node>
+<node TEXT="rhythmic" ID="ID_988353992" CREATED="1600436282398" MODIFIED="1600436293163">
+<node TEXT="remove need to decide when to go deep" ID="ID_798295091" CREATED="1600436377413" MODIFIED="1600436388805"/>
+<node TEXT="visual aid of chain method" ID="ID_914538997" CREATED="1600436395800" MODIFIED="1600436412560"/>
+</node>
+<node TEXT="journalistic" ID="ID_533513508" CREATED="1600436426848" MODIFIED="1600436432433">
+<node TEXT="fit deep work wherever you can" ID="ID_828212609" CREATED="1600436457046" MODIFIED="1600436463821"/>
+</node>
+</node>
+<node TEXT="rituals" ID="ID_1170863128" CREATED="1600436471692" MODIFIED="1600436474597">
+<node TEXT="where, and how long" ID="ID_661871556" CREATED="1600436501511" MODIFIED="1600436505207"/>
+<node TEXT="how you work" ID="ID_704681183" CREATED="1600436511176" MODIFIED="1600436513491"/>
+<node TEXT="food, drinks, walks etc" ID="ID_1040601978" CREATED="1600436563500" MODIFIED="1600436569250"/>
+<node TEXT="invest" ID="ID_1517897958" CREATED="1600436592081" MODIFIED="1600436602279"/>
+</node>
+<node TEXT="limit willpower drain" ID="ID_1468703779" CREATED="1600436536075" MODIFIED="1600436546085"/>
+<node TEXT="business execution" ID="ID_1618684073" CREATED="1600436631211" MODIFIED="1600436637386">
+<node TEXT="focus on important goals" ID="ID_1032625311" CREATED="1600436638103" MODIFIED="1600436665625"/>
+<node TEXT="act on lead measures, not lag measures" ID="ID_380069559" CREATED="1600436665931" MODIFIED="1600436686738"/>
+<node TEXT="keep score board" ID="ID_412272451" CREATED="1600436693332" MODIFIED="1600436696781"/>
+<node TEXT="confront owners with scoreboard and actions to improve weekly" ID="ID_673220944" CREATED="1600436833152" MODIFIED="1600436851565"/>
+</node>
+<node TEXT="downtime" ID="ID_54809637" CREATED="1600436877606" MODIFIED="1600436880204">
+<node TEXT="create insight" ID="ID_1783486418" CREATED="1600436883223" MODIFIED="1600436891781"/>
+<node TEXT="recharge energy" ID="ID_1002796938" CREATED="1600436897089" MODIFIED="1600436901329"/>
+<node TEXT="do not work in the evening as well" ID="ID_392009855" CREATED="1600436911203" MODIFIED="1600436922016"/>
+<node TEXT="shutdown routine" ID="ID_858959714" CREATED="1600435944978" MODIFIED="1600435948804"/>
+</node>
+</node>
+<node TEXT="embrace boredom 155" ID="ID_1620444408" CREATED="1600435657710" MODIFIED="1600435826927">
+<node TEXT="train" ID="ID_1754465032" CREATED="1600437093350" MODIFIED="1600437106935">
+<node TEXT="intense concentration" ID="ID_347518550" CREATED="1600437108007" MODIFIED="1600437118064">
+<node TEXT="work intense" ID="ID_980958806" CREATED="1600437269977" MODIFIED="1600437274914"/>
+<node TEXT="meditate when possible" ID="ID_1416708535" CREATED="1600437275701" MODIFIED="1600437280967"/>
+<node TEXT="memorize deck of cards" ID="ID_1251823499" CREATED="1600437291185" MODIFIED="1600437298492"/>
+</node>
+<node TEXT="battle distraction" ID="ID_299464169" CREATED="1600437118369" MODIFIED="1600437128070">
+<node TEXT="block internet" ID="ID_1456492396" CREATED="1600437169594" MODIFIED="1600437173030"/>
+</node>
+</node>
+</node>
+<node TEXT="quit social media 181" ID="ID_1632977489" CREATED="1600435663091" MODIFIED="1600435831578"/>
+<node TEXT="drain the shallows 215" ID="ID_578828253" CREATED="1600435666513" MODIFIED="1600435835885"/>
+</node>
+</node>
+</node>
+</map>
